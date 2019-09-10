b854a5ac23f789e9f57c1a48d054a5dba255df77
Sunny Tong <50933364+SunnyTong2019@users.noreply.github.com>
2019-09-09 22:09:09 -0700

    added code to request gif and display gif when button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,11 +8,11 @@ $(document).ready(function () {
         $("#buttons-view").empty();
 
         for (var i = 0; i < reactions.length; i++) {
-            var a = $("<button>");
-            a.addClass("btn btn-info m-2 reaction-button");
-            a.attr("data-reaction", reactions[i]);
-            a.text(reactions[i]);
-            $("#buttons-view").append(a);
+            var newButton = $("<button>");
+            newButton.addClass("btn btn-info m-2 reaction-button");
+            newButton.attr("data-reaction", reactions[i]);
+            newButton.text(reactions[i]);
+            $("#buttons-view").append(newButton);
         }
     }
 
@@ -31,7 +31,38 @@ $(document).ready(function () {
 
     renderButtons();
 
+    $(".reaction-button").on("click", function () {
 
-   
+        var reaction = $(this).attr("data-reaction");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&q=" + reaction + "&limit=10";
 
-});
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response);
+            var results = response.data;
+
+            for (var i = 0; i < results.length; i++) {
+                var stillURL = results[i].images.fixed_height_still.url;
+                var animateURL = results[i].images.fixed_height.url;
+
+                var newDiv = $("<div>");
+                newDiv.addClass("p-2");
+                var newImg = $("<img>");
+                newImg.attr("src", stillURL);
+                newImg.attr("data-still", stillURL);
+                newImg.attr("data-animate", animateURL);
+                newImg.attr("data-state", "still");
+                newImg.addClass("gif");
+                newDiv.append(newImg);
+                $("#gifs-view").append(newDiv);
+            }
+
+                });
+        
+            })
+        
+        
+        
+        });
